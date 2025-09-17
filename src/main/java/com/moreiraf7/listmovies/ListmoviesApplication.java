210c66c98ec2ebab990f5f18e09b350705a03082
Antônio Felipe <antoniofelipemoreiraf7@gmail.com>
2025-09-17 17:21:54 -0300

    Projeto criado

---
+++ src/main/java/com/moreiraf7/listmovies/ListmoviesApplication.java
@@ -0,0 +1,13 @@
+package com.moreiraf7.listmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListmoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moreiraf7/listmovies/ListmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moreiraf7.listmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
