c130e02093a61b828a37ac80d8a761cbd82ab1a1
Antônio Felipe <antoniofelipemoreiraf7@gmail.com>
2025-09-18 15:36:13 -0300

    Implementando MovieRepository

---
+++ src/main/java/com/moreiraf7/listmovies/repository/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.moreiraf7.listmovies.repository;
+
+import com.moreiraf7.listmovies.entities.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//Interface responsável pelo acesso ao banco de dados, e as implementações do JpaRepository
+public interface MovieRepository extends JpaRepository<Movie,Long> {
+    
+}
